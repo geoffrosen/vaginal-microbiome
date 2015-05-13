d85494c9cbbdedab0a5da7b108d5462a56f26024
Geoff Rosen <geoff.rosen@gmail.com>
2015-05-12 22:52:40 -0700

    added redup, made modify able to insert multiple headers

--- modify_otu_table.py
+++ modify_otu_table.py
@@ -9,11 +9,13 @@ def main():
 	parser.add_argument('-o','--output-fp',help='output file path',required=True)
 	parser.add_argument('-s','--start-id',help='the id as labelled in the input file', required=True)
 	parser.add_argument('-e','--end-id',help='the id you want in the output file',required=True)
-	parser.add_argument('-c','--class-name',help='the class you want to be used',required=True)
+	parser.add_argument('-c','--class-names',help='the classes you want to be used, split by commas',required=True)
 	parser.add_argument('-l','--level',help='change depth of taxonomy',default=-1,type=int)
+	parser.add_argument('--exact-level',help='only use the exact level specified above',action='store_true',default=False)
 	parser.add_argument('-n','--normalize',help='normalize by column',action='store_true',default=False)
 	#need to think through how bottom will work with level. maybe can only have one
-	parser.add_argument('-b','--bottom',help='will only return the bottom listing (with above heirarchy)',default=False,action='store_true')
+	parser.add_argument('-b','--bottom',help='will only return the bottom listing (with above heirarchy in name)',default=False,action='store_true')
+	parser.add_argument('--no-split',help='will use the terms directly as input',default=False,action='store_true')
 	parser.add_argument('--in-splitter',help='the splitter as the files come in',default=';')
 	parser.add_argument('--out-splitter',help='the splitter you want as the files go out',default='|')
 	parser.add_argument('--otu-min',help='the minimum (as a decimal for normalize or count number) for an otu to be included',default=float(0),type=float)
@@ -28,12 +30,18 @@ def main():
 			if i == 0:
 				start_col = row.index(args.start_id)
 				new_col = row.index(args.end_id)
-				class_col = row.index(args.class_name)
+				class_col = []
+				for cn in args.class_names.split(','):
+					class_col.append(row.index(cn))
 				i = 1
 			else:
-				mapper[row[start_col]] = (row[new_col],row[class_col])
+				mapper[row[start_col]] = [row[new_col]]
+				for cl in class_col:
+					mapper[row[start_col]].append(row[cl])
 		i = 0
-		class_row = [args.class_name]
+		class_rows = []
+		for cn in args.class_names.split(','):
+			class_rows.append([cn])
 		new_row = [args.end_id]
 		if args.level:
 			holder = Family(args.out_splitter)
@@ -41,7 +49,7 @@ def main():
 			if i == 0:
 				for item in row[1:]:
 					try:
-						(this_new_id, this_class) = mapper[item]
+						this_info = mapper[item]
 					except:
 						if len(row) - row.index(item) > 2:
 							print 'failed because mapper didn\'t contain %s' % item
@@ -49,27 +57,32 @@ def main():
 						else:
 							print 'failed on %s' % item
 							continue
-					new_row.append(this_new_id)
-					class_row.append(this_class)
-				ww.writerow(class_row)
+					new_row.append(this_info[0])
+					#this seems dangerous
+					for q in range(1,len(this_info)):
+						class_rows[q-1].append(this_info[q])
+				for cr in class_rows:
+					ww.writerow(cr)
 				ww.writerow(new_row)
 				i = 1
 			else:
 				if args.level:
-					this_name = row[-1].split(args.in_splitter)
-					this_name = filter(None, this_name)
-					for j in range(len(this_name)):
-						this_name[j] = this_name[j].strip()
-					this_name = args.out_splitter.join(this_name)
+					if args.no_split:
+						this_name = [row[-1]]
+					else:
+						this_name = row[-1].split(args.in_splitter)
+						this_name = filter(None, this_name)
+						for j in range(len(this_name)):
+							this_name[j] = this_name[j].strip()
 					holder.append(this_name,row[1:-1])
 				else:
 					ww.writerow(row)
 		if args.level:
 			if args.normalize:
-				otu_min = args.otu_min * float(len(class_row) - 1)
+				otu_min = args.otu_min * float(len(class_rows[0]) - 1)
 			else:
 				otu_min = args.otu_min * float(holder.sum_counts())
-			for row in holder.out(normalize=args.normalize,level=args.level,minimum=otu_min,bottom=args.bottom):
+			for row in holder.out(normalize = args.normalize, level = args.level, minimum = otu_min, bottom=args.bottom, exact_level=args.exact_level):
 				ww.writerow(row)
 
 
@@ -88,15 +101,15 @@ class Clade(object):
 		else:
 			for i in range(len(incounts)):
 				self.__counts[i] += float(incounts[i])
-		split_name = name.split(self.__splitter)
+		split_name = name
 		self.__name = split_name[0]
 
 		if len(split_name) > 1:
 			if split_name[1] not in self.__sub:
 				self.__sub[split_name[1]] = Clade(self.__splitter)
-			self.__sub[split_name[1]].append(self.__splitter.join(split_name[1:]),incounts)
+			self.__sub[split_name[1]].append(split_name[1:],incounts)
 
-	def out_array(self,par=False,normalize=False,parcounts=False,level=-1,minimum=0,bottom=False):
+	def out_array(self,par=False,normalize=False,parcounts=False,level=-1,minimum=0,bottom=False,exact_level=False):
 		if level == 0:
 			return []
 		if par:
@@ -122,13 +135,20 @@ class Clade(object):
 			my_counts = self.__counts
 		if sum(my_counts) < minimum:
 			return []
-		out = [[my_name] + my_counts]
+		if exact_level and level == 1:
+			return [[my_name] + my_counts]
+		elif exact_level and level != 1:
+			#statement is a bit redundant
+			out = []
+		elif bottom and len(self.__sub) != 0:
+			out = []
+		else:
+			out = [[my_name] + my_counts]
 		if parcounts:
 			pass_counts = list(parcounts)
 		else:
 			pass_counts = list(self.__counts)
-		if not bottom:
-			for sub in self.__sub:
+		for sub in self.__sub:
 				out += self.__sub[sub].out_array(my_name,normalize,list(pass_counts),level=level-1,minimum=minimum,bottom=bottom)
 		return out
 		
@@ -143,7 +163,7 @@ class Family(object):
 		self.__counts = None
 
 	def append(self, name, incounts):
-		child_name = name.split(self.__splitter)[0]
+		child_name = name[0]
 		if child_name not in self.__sub:
 			self.__sub[child_name] = Clade(self.__splitter)
 		self.__sub[child_name].append(name,incounts)
@@ -156,10 +176,10 @@ class Family(object):
 	def sum_counts(self):
 		return sum(self.__counts)
 
-	def out(self,normalize=False,level=-1,minimum=float(0),bottom=False):
+	def out(self,normalize=False,level=-1,minimum=float(0),bottom=False,exact_level=False):
 		out = []
 		for sub in self.__sub:
-			out += self.__sub[sub].out_array(normalize=normalize,parcounts=list(self.__counts),level=level,minimum=minimum,bottom=bottom)
+			out += self.__sub[sub].out_array(normalize=normalize,parcounts=list(self.__counts),level=level,minimum=minimum,bottom=bottom,exact_level=False)
 		return out
 
 if __name__ == '__main__':
---
+++ redup_from_csv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import argparse, csv, sys
+
+def main():
+	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('-i','--input-csv',help='the input csv that you would like to redup', required=True)
+	parser.add_argument('-o','--output-csv',help='the output csv location',required=True)
+	parser.add_argument('-d','--dedup-fp',help='filepath to dedup', required=True)
+	args = parser.parse_args()
+	redup(args.input_csv, args.output_csv, args.dedup_fp)
+
+def redup(input_csv, output_csv, dedup_fp):
+	dups = {}
+	with open(dedup_fp,'rb') as dedup:
+		r = csv.reader(dedup,delimiter=',')
+		for [orig, dup, num] in r:
+			if orig not in dups:
+				dups[orig] = []
+			dups[orig].append(dup)
+	with open(input_csv,'rb') as incsv, open(output_csv,'wb') as outcsv:
+		r = csv.reader(incsv,delimiter=',')
+		w = csv.writer(outcsv,delimiter=',')
+		i = 0
+		for row in r:
+			if i == 0:
+				i = 1
+				w.writerow(row)
+			else:
+				for other in dups[row[0]]:
+					w.writerow([other] + row[1:])
+				w.writerow(row)
+
+if __name__ == '__main__':
+	main()
